bb877a346941eff00b405e65346e90b9e895a162
Smit Vaghasiya <125259398+SmitVaghasiya24@users.noreply.github.com>
2024-12-20 20:29:40 +0530

    Add files via upload

---
+++ src/components/Shop.jsx
@@ -0,0 +1,15 @@
+import list from "../list"
+import Card from "./Card"
+// import "../style/card.css"
+
+const Shop = ({handleClick}) => {
+  return (
+ <section>
+    {list.map((item)=>{
+        return <Card item={item} key={item.id} handleClick={handleClick}/>
+})}
+ </section>
+  )
+}
+
+export default Shop
